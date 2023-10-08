e214ed898600e7f70f448da9ebd6cc7d04e8f618
junw <wo1261931780@gmail.com>
2023-10-08 20:26:46 +0800

    初始化

---
+++ src/main/java/wo1261931780/testSpringCloud/TestSpringCloudApplication.java
@@ -0,0 +1,13 @@
+package wo1261931780.testSpringCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wo1261931780/testSpringCloud/TestSpringCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package wo1261931780.testSpringCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
