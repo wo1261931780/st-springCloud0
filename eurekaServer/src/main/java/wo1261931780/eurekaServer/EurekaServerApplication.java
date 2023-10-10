735d08db985b40d2cb8e8707624a9c4a4256b752
junw <wo1261931780@gmail.com>
2023-10-10 14:26:15 +0800

    1

--- eurekaServer/src/main/java/wo1261931780/eurekaServer/EurekaServerApplication.java
+++ eurekaServer/src/main/java/wo1261931780/eurekaServer/EurekaServerApplication.java
@@ -10,7 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
-
+	// @EnableEurekaServer
+	// 该注解表明这是一个EurekaServer，提供服务注册和服务发现功能
+	//eureka
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServerApplication.class, args);
 	}
--- gatewayServer/src/test/java/wo1261931780/gatewayServer/GatewayServerApplicationTests.java
+++ gatewayServer/src/test/java/wo1261931780/gatewayServer/GatewayServerApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GatewayServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
